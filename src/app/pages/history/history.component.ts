d6681a5cab0a0be4efb82090aa03cb19de794eea
sarahvar <sarahvarin95@gmail.com>
2024-12-05 18:19:56 +0100

    Histoire-Arcane

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -20,9 +20,17 @@ export class HistoryComponent {
     { path: '/viktor', name: 'Viktor' }
   ];
 
+  arcaneStory: string = `
+    La série Arcane, disponible sur Netflix, est une adaptation de l'univers de League of Legends. Elle raconte une histoire captivante ancrée dans le conflit entre deux cités jumelles : Piltover, une ville brillante et prospère, et Zaun, son opposé sombre et industrialisé.
+    L'intrigue explore les origines de plusieurs personnages emblématiques du jeu, notamment Vi, Jinx, Caitlyn, Jayce et Viktor.
+
+    L'intrigue principale tourne autour de Violet (Vi) et Powder (Jinx), deux sœurs aux relations complexes marquées par la tragédie, l'amour et la perte. Ce duo est central dans un univers où la lutte pour le pouvoir, la quête de justice, et les découvertes technologiques comme l'Hextech transforment leur monde. Jayce et Viktor, par exemple, explorent le potentiel de cette magie technologique, avec des conséquences éthiques et personnelles profondes.
+
+    Arcane se distingue par une narration intimiste, où les relations entre les personnages – qu'elles soient amicales, familiales, ou conflictuelles – sont au cœur du récit, offrant une profondeur émotionnelle rare dans les séries d'animation.
+  `;
+
   constructor() {
     // Pas besoin de récupérer les routes dynamiquement du Router
     // Vous pouvez directement manipuler le tableau statique ci-dessus
   }
 }
-
