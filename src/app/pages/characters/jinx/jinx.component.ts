d6e3b1477609ed1aeb62bbc462477a1bf1cc68c7
sarahvar <sarahvarin95@gmail.com>
2024-12-05 22:44:29 +0100

    Jinx

--- src/app/pages/characters/jinx/jinx.component.ts
+++ src/app/pages/characters/jinx/jinx.component.ts
@@ -17,6 +17,24 @@ routes: { path: string; name: string }[] = [
   { path: '/jayce', name: 'Jayce' },
   { path: '/viktor', name: 'Viktor' }
 ];
+title = 'Jinx - La Fille de la Folie';
+
+  jinxBio = `Jinx est un personnage emblématique de l'univers d'Arcane et de League of Legends. Dans la série Arcane, elle est l'antagoniste principale dans la région de Piltover et Zaun. Elle est connue pour ses explosions colorées, ses jouets dangereux et son comportement imprévisible, ce qui la rend à la fois intrigante et terrifiante.
+
+  Jinx a un passé marqué par la souffrance et la perte, ce qui a forgé sa personnalité chaotique et déstabilisée. D'abord innocente et attachée à sa sœur Vi, elle a traversé de nombreux événements traumatisants qui ont contribué à sa transformation en l'un des personnages les plus emblématiques de l'univers de League of Legends.`;
+
+  jinxPowers = [
+    'Explosions et armes de destruction massive',
+    'Grande agilité et maîtrise des armes',
+    'Comportement imprévisible et extrêmement dangereux'
+  ];
+
+  // Liste des apparitions dans la série
+  appearances = [
+    'Saison 1 - Episode 1 : Apparition en tant qu\'enfant',
+    'Saison 1 - Episode 3 : Première rencontre avec Vi en tant qu\'adulte',
+    'Saison 2 - Episode 8 : Jinx devient un protagoniste central'
+  ];
 constructor() {
   // Pas besoin de récupérer les routes dynamiquement du Router
   // Vous pouvez directement manipuler le tableau statique ci-dessus
