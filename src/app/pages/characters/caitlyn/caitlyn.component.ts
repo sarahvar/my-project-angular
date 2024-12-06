426b6a173a4a8daab8440eced6225f848acd02c0
sarahvar <sarahvarin95@gmail.com>
2024-12-06 21:42:11 +0100

    Caitlyn

--- src/app/pages/characters/caitlyn/caitlyn.component.ts
+++ src/app/pages/characters/caitlyn/caitlyn.component.ts
@@ -12,9 +12,13 @@ import { CommonModule } from '@angular/common';  // Importation de CommonModule
 
   export class CaitlynComponent {
     title = 'Caitlyn - La Shérif de Piltover ⭐🤠';
-    bio = `Caitlyn est une détective brillante de Piltover. Réputée pour sa
-            détermination et son sens de la justice, elle s'allie souvent avec Vi
-            pour résoudre les mystères les plus complexes et défendre sa ville.`;
+    bio = `Caitlyn Kiramman est issue d'une famille noble de Piltover, la Cité du Progrès. Cependant, son privilège familial ne l'a pas isolée de la réalité complexe de sa ville. Après qu'un drame personnel a frappé sa famille, Caitlyn a décidé de se dédier à la protection des innocents et à la lutte contre la corruption dans Piltover.
+
+Grâce à ses compétences exceptionnelles en enquête, son sang-froid et sa maîtrise des armes à feu, elle est rapidement devenue la meilleure détective de la ville. Avec son fusil Hextech, conçu pour la précision et l'efficacité, elle traque les criminels les plus dangereux, faisant d'elle une figure respectée et redoutée.
+
+Dans la série Arcane, Caitlyn joue un rôle crucial en s'alliant avec Vi. Leur relation, marquée par des différences de classe sociale et de caractère, évolue ❤️. Ensemble, elles affrontent des ennemis redoutables tout en dévoilant les complots sombres qui menacent Piltover et Zaun.
+
+Au-delà de son rôle de shérif, Caitlyn incarne une femme forte, indépendante et animée par une véritable volonté de changement. Son dévouement, sa morale inébranlable et sa volonté de voir au-delà des préjugés sociaux font d'elle une héroïne respectée et inspirante.`;
 
     abilities = ['Tir de précision', 'Pièges tactiques', 'Sniper d\'élite'];
     series = ['Saison 1 - Épisodes 3 à 9'];
