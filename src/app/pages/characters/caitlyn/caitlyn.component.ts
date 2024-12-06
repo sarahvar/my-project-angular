05ad78b8c0642e87fe7657dc2a67c19baac7f462
sarahvar <sarahvarin95@gmail.com>
2024-12-06 22:04:48 +0100

    Caitlyn

--- src/app/pages/characters/caitlyn/caitlyn.component.ts
+++ src/app/pages/characters/caitlyn/caitlyn.component.ts
@@ -16,12 +16,13 @@ import { CommonModule } from '@angular/common';  // Importation de CommonModule
 
 Grâce à ses compétences exceptionnelles en enquête, son sang-froid et sa maîtrise des armes à feu, elle est rapidement devenue la meilleure détective de la ville. Avec son fusil Hextech, conçu pour la précision et l'efficacité, elle traque les criminels les plus dangereux, faisant d'elle une figure respectée et redoutée.
 
-Dans la série Arcane, Caitlyn joue un rôle crucial en s'alliant avec Vi. Leur relation, marquée par des différences de classe sociale et de caractère, évolue ❤️. Ensemble, elles affrontent des ennemis redoutables tout en dévoilant les complots sombres qui menacent Piltover et Zaun.
+Dans la série Arcane, Caitlyn joue un rôle crucial en s'alliant avec Vi. Leur relation, marquée par des différences de classe sociale et de caractère, évolue lors de la saison 2 ❤️. Ensemble, elles affrontent des ennemis redoutables tout en dévoilant les complots sombres qui menacent Piltover et Zaun.
 
 Au-delà de son rôle de shérif, Caitlyn incarne une femme forte, indépendante et animée par une véritable volonté de changement. Son dévouement, sa morale inébranlable et sa volonté de voir au-delà des préjugés sociaux font d'elle une héroïne respectée et inspirante.`;
 
     abilities = ['Tir de précision', 'Pièges tactiques', 'Sniper d\'élite'];
-    series = ['Saison 1 - Épisodes 3 à 9'];
+    series = ['Saison 1 - Épisodes 3 à 9',
+             'Saison 2 - Épisodes 1 à 9'];
   // Définir un tableau statique de routes avec un 'path' et un 'name'
   routes: { path: string; name: string }[] = [
     { path: '/', name: 'Accueil' },
