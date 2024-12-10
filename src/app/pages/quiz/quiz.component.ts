2b9b997ba632de32daca660162462c21c67e4cbc
sarahvar <sarahvarin95@gmail.com>
2024-12-10 13:53:53 +0100

    Quiz

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Question } from './quiz.model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-quiz',
   standalone: true,
@@ -14,22 +15,26 @@ export class QuizComponent {
   selectedAnswer: string = '';
   score: number = 0;
   showResult: boolean = false;
+  showExplanation: boolean = false;
 
   questions: Question[] = [
     {
       questionText: 'Quel est le nom de la sœur de Jinx ?',
       options: ['Vi', 'Caitlyn', 'Mel', 'Jayce'],
-      correctAnswer: 'Vi'
+      correctAnswer: 'Vi',
+      explanation: 'Vi est la sœur de Jinx, elles ont grandi ensemble à Zaun.'
     },
     {
       questionText: 'Où se déroule principalement l\'action de la série Arcane ?',
       options: ['Zaun', 'Ionia', 'Noxus', 'Demacia'],
-      correctAnswer: 'Zaun'
+      correctAnswer: 'Zaun',
+      explanation: 'La majeure partie de l\'action de la série Arcane se déroule dans la ville souterraine de Zaun.'
     },
     {
       questionText: 'Quel est le talent principal de Viktor ?',
       options: ['Technologie', 'Magie', 'Combat', 'Politique'],
-      correctAnswer: 'Technologie'
+      correctAnswer: 'Technologie',
+      explanation: 'Viktor est connu pour ses compétences en technologie et ses inventions.'
     },
   ];
 
@@ -41,12 +46,23 @@ export class QuizComponent {
     if (this.selectedAnswer === this.questions[this.currentQuestionIndex].correctAnswer) {
       this.score++;
     }
+    // Stocker la réponse de l'utilisateur
+    this.questions[this.currentQuestionIndex].userAnswer = this.selectedAnswer;
+    this.showExplanation = true;
+  }
+
+  nextQuestion() {
     this.currentQuestionIndex++;
     this.selectedAnswer = '';
+    this.showExplanation = false;
 
     if (this.currentQuestionIndex >= this.questions.length) {
       this.showResult = true;
     }
+  }
+
+  getExplanation(): string {
+    return this.questions[this.currentQuestionIndex - 1]?.explanation || '';
   }
 }
 
--- src/app/pages/quiz/quiz.model.ts
+++ src/app/pages/quiz/quiz.model.ts
@@ -1,6 +1,8 @@
-// question.model.ts
+
 export interface Question {
   questionText: string;
   options: string[];
   correctAnswer: string;
+  explanation: string;
+  userAnswer?: string; // Optionnel pour stocker la réponse de l'utilisateur
 }
