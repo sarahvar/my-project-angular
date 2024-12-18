94ac16691d10870d2f31f5c3cb58cf040611884f
sarahvar <sarahvarin95@gmail.com>
2024-12-18 19:18:34 +0100

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,13 +18,13 @@ export class HomeComponent {
     { path: 'caitlyn', name: 'Caitlyn' },
     { path: 'jayce', name: 'Jayce' },
     { path: 'viktor', name: 'Viktor' },
-    { path: '/silco', name: 'Silco' },
-    { path: '/ekko', name: 'Ekko' },
-    { path: '/isha', name: 'Isha' },
-    { path: '/quiz', name: 'Quiz' },
-    { path: '/game', name: 'Memory Game' },
-    { path: '/fight', name: 'Fight' },
+    { path: 'silco', name: 'Silco' },   // Retirer le '/' ici
+    { path: 'ekko', name: 'Ekko' },     // Retirer le '/' ici
+    { path: 'isha', name: 'Isha' },     // Retirer le '/' ici
+    { path: 'quiz', name: 'Quiz' },
+    { path: 'game', name: 'Memory Game' },
+    { path: 'fight', name: 'Fight' },
   ];
-  title = 'Bienvenue dans le monde d\''; // Définir seulement le texte du titre ici
-}
 
+  title = 'Bienvenue dans le monde d\'Arcane'; // Titre de la page d'accueil
+}
