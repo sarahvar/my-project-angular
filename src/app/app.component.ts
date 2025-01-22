9e899df001ed457b45710fffdd242671ce0d5ba1
sarahvar <sarahvarin95@gmail.com>
2025-01-22 19:32:27 +0100

    haut-de-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 // app.component.ts
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, NavigationEnd, Router  } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html', // On va modifier le template ici
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Écouter les changements de route
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd) // On filtre uniquement les événements de fin de navigation
+    ).subscribe(() => {
+      // Une fois la navigation terminée, on fait défiler la page vers le haut
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
 }
