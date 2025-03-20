0988ab1d9f54c69c10f3cab8f8ef7e70987426db
sarahvar <sarahvarin95@gmail.com>
2025-03-20 21:00:47 +0100

    confidentialite

--- src/app/pages/confidentialite/confidentialite.component.ts
+++ src/app/pages/confidentialite/confidentialite.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';  // Importation de RouterModule
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-confidentialite',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './confidentialite.component.html',
   styleUrl: './confidentialite.component.scss'
 })
 export class ConfidentialiteComponent {
-
+  routes: { path: string; name: string }[] = [
+    { path: '/', name: 'Accueil 🏠' },
+    { path: '/histoire', name: 'L\'histoire 📖'},
+    { path: '/vi', name: 'Vi 👊' },
+    { path: '/jinx', name: 'Jinx 💥' },
+    { path: '/caitlyn', name: 'Caitlyn ⭐🤠' },
+    { path: '/jayce', name: 'Jayce ⚙️' },
+    { path: '/viktor', name: 'Viktor 🔧' },
+    { path: '/silco', name: 'Silco 🐍' },
+    { path: '/ekko', name: 'Ekko ⏳' },
+    { path: '/isha', name: 'Isha 🌱' },
+    { path: '/vander', name: 'Vander 🍺🐺'},
+    { path: '/sevika', name: 'Sevika 🦾'},
+    { path: '/mel', name: 'Mel 🌟'},
+    { path: '/quiz', name: 'Quiz 📝' },
+    { path: '/game', name: 'Memory Game 🧠' },
+    { path: '/fight', name: 'Fight 🥊' },
+  ];
 }
